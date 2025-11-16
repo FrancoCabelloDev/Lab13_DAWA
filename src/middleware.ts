0cb32cf0d48ef21009592114ab4b4423fd7e550c
FrancoCabelloDev <franco_alex_07@hotmail.com>
2025-11-15 23:35:40 -0500

    se modifico middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,20 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    const signInUrl = new URL("/signin", request.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/dashboard", "/profile"],
